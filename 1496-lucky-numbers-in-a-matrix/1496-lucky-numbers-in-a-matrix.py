72061eddaebaedcda47c14ef9bdf50749436981b
Thirupathi <115494239+Thirupathi-702@users.noreply.github.com>
2024-07-22 22:28:37 +0530

    Time: 108 ms (78.03%), Space: 16.9 MB (20.7%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
@@ -1,27 +1,23 @@
 class Solution:
     def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
-        q=[]
-        r=[]
-        l1=[]
+        row=[]
+        col=[]
+        ans=[]
         
-        for i in range(len(matrix)):
-            l=-1
+        for i in range(len(matrix)):  
             h=float('inf')
             for j in range(len(matrix[0])):
                 if h>matrix[i][j]:
                     h=matrix[i][j]
-                    l=i
-            if l!=-1:
-                l1.append(h)
+            row.append(h)
         for i in range(len(matrix[0])):
             p=-1
             for j in range(len(matrix)):
                 if p<matrix[j][i]:
                     p=matrix[j][i]
-            q.append(p)
-                
-        for i in q:
-            if i in l1:
-                r.append(i)
-        return r
+            col.append(p)      
+        for i in col:
+            if i in row:
+                ans.append(i)
+        return ans
                 
