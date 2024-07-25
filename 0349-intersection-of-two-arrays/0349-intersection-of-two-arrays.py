3ea8114eea97a5711a438a727819cb066d1de427
Thirupathi <115494239+Thirupathi-702@users.noreply.github.com>
2024-07-25 20:38:27 +0530

    Time: 55 ms (22.45%), Space: 16.8 MB (21.51%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,7 +1,13 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        c=[]
+        res=[]
+        c=[0]*1001
+        d=[0]*1001
         for i in nums1:
-            if i in nums2 and i not in c:
-                c.append(i)
-        return c
+            c[i]=1
+        for i in nums2:
+            d[i]=1
+        for i in range(len(c)):
+            if c[i]==1 and d[i]==1:
+                res.append(i)
+        return res
