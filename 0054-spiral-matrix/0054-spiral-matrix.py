b66c463bc7923d72102ea1b406874a5e1871a64b
Thirupathi <115494239+Thirupathi-702@users.noreply.github.com>
2024-08-08 11:18:05 +0530

    Time: 42 ms (12.79%), Space: 16.6 MB (9.04%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,24 +1,60 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        left=0
-        top=0
-        n=[]
+        l=0
+        r=len(matrix[0])-1
         bottom=len(matrix)-1
-        right=len(matrix[0])-1
-        while left<=right and top<=bottom:
-            for i in range(left,right+1):
-                n.append(matrix[top][i])
+        top=0
+        m=[]
+        while l<=r and top<=bottom:
+            for i in range(l,r+1):
+                m.append(matrix[top][i])
             top+=1
+            
             for i in range(top,bottom+1):
-                n.append(matrix[i][right])
-            right-=1
-            if len(n)==(len(matrix)*len(matrix[0])):
-                break
-            for i in range(right,left-1,-1):
-                n.append(matrix[bottom][i])
-            bottom-=1
+                m.append(matrix[i][r])
+            r-=1
+            if l<=r and top<=bottom:
+                for i in range(r,l-1,-1):
+                    m.append(matrix[bottom][i])
+                bottom-=1
+                
+                for i in range(bottom,top-1,-1):
+                    m.append(matrix[i][l])
+                l+=1
+        return m
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # left=0
+        # top=0
+        # n=[]
+        # bottom=len(matrix)-1
+        # right=len(matrix[0])-1
+        # while left<=right and top<=bottom:
+        #     for i in range(left,right+1):
+        #         n.append(matrix[top][i])
+        #     top+=1
+        #     for i in range(top,bottom+1):
+        #         n.append(matrix[i][right])
+        #     right-=1
+        #     if len(n)==(len(matrix)*len(matrix[0])):
+        #         break
+        #     for i in range(right,left-1,-1):
+        #         n.append(matrix[bottom][i])
+        #     bottom-=1
         
-            for i in range(bottom,top-1,-1):
-                n.append(matrix[i][left])
-            left+=1
-        return n
+        #     for i in range(bottom,top-1,-1):
+        #         n.append(matrix[i][left])
+        #     left+=1
+        # return n
