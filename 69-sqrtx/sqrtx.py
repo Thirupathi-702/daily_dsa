c68356ec288051b37bc6f90894d0f3f6e83e9203
Thirupathi <115494239+Thirupathi-702@users.noreply.github.com>
2024-09-10 17:35:10 +0530

    Time: 43 ms (41.08%) | Memory: 16.5 MB (65.86%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,8 +1,16 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         i=0
+        if x==0:
+            return 0
+        j=x
         ans=0
-        while i*i<=x:
-            ans=i
-            i+=1
+        while i<=j:
+            mid=(i+j)//2
+            if mid*mid>x:
+                
+                j=mid-1
+            else:
+                i=mid+1
+                ans=mid
         return ans
