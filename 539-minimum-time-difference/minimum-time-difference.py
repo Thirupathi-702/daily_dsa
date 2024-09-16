0bd60f6d420cead4e69cdae12840e36b62403ef7
Thirupathi <115494239+Thirupathi-702@users.noreply.github.com>
2024-09-16 21:04:17 +0530

    Time: 63 ms (86.31%) | Memory: 19.6 MB (36.73%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.py
+++ 539-minimum-time-difference/minimum-time-difference.py
@@ -14,10 +14,7 @@ class Solution:
         for i in range(len(a)):
             f=a[(i+1)%n]
             se=a[i%n]
-            least_time2=abs((1440+f)-se)%1440
-            least_time=abs(f-(se+1440))%1440
-            k=min(k,least_time,least_time2)
-            #k=min(k,1440-k,k-0)
-       
+            k=min(k,abs(f-se))
+        k=min(k,1440-a[-1]+a[0])
         return k
         
