d8853384442dfdb9434362ff2a5803743b0e9cc8
Thirupathi <21r05a6702@gmail.com>
2024-12-02 17:55:55 +0530

    Time: 0 ms (100%), Space: 17.1 MB (5.67%) - LeetHub

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,19 +1,19 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        a=1
-        p=1
-        w=0
+        word_count=1
+        prefix=1
+        search_idx=0
         for i in sentence:
             if i==" ":
-                a+=1
-                p=1
-                w=0
-            elif i==searchWord[w] and p==1:
-                if w==len(searchWord)-1:
-                    return a
+                word_count+=1
+                prefix=1
+                search_idx=0
+            elif i==searchWord[search_idx] and prefix==1:
+                if search_idx==len(searchWord)-1:
+                    return word_count
                 else:
-                    w+=1
+                    search_idx+=1
             else:
-                p=0
-                w=0
+                prefix=0
+                search_idx=0
         return -1
